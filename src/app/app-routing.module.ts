06365f1a1664e10c9743fbcebd04ded2f4d53430
David <david.lopes@akross.com.br>
2023-06-13 11:08:48 -0300

    one more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'home' },
+
 ];
 
 @NgModule({
