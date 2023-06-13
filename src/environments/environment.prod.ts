0c4f78ca735bd0551d7ee4a940f25be659e36831
David <david.lopes@akross.com.br>
2023-06-13 09:53:01 -0300

    adding build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  googleAppsScriptBackend: "https://script.google.com/macros/s/AKfycbztW4HapfEyZL3sEReRvJB7PELd3oXeW_kgqgVHnMZqKDRoG2fW13msapm2onaH-e91_A/exec",
   production: true
 };
