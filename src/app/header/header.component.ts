63f8a52703523dccce09a7be71707f804ee40b52
David <david.lopes@akross.com.br>
2023-06-26 14:13:31 -0300

    fix on logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,7 @@ import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalHelperComponent } from '../modal-helper/modal-helper.component';
 import { Router } from '@angular/router';
+import { UserService } from 'src/services/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,11 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private dialog: MatDialog, private userSocialService:SocialAuthService,private router: Router) {
+  constructor(private dialog: MatDialog, private userSocialService:SocialAuthService,private router: Router,private userService: UserService,) {
   }
 
   callLogout() {
-    this.userSocialService.signOut();
+    this.userService.signOut();
     this.router.navigate(['/login']);
   }
 
